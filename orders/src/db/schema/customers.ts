f653e1e040ce2826a019189c9c3d468db38a5816
Bruno Monteiro <brunomonteiro3@outlook.com.br>
2025-06-07 09:55:24 -0300

    feat: create order and invoices services

---
+++ orders/src/db/schema/customers.ts
@@ -0,0 +1,19 @@
+import {
+  integer,
+  pgEnum,
+  text,
+  pgTable,
+  timestamp,
+  date,
+} from "drizzle-orm/pg-core";
+
+export const customers = pgTable("customers", {
+  id: text().primaryKey(),
+  name: text().notNull(),
+  email: text().notNull().unique(),
+  address: text().notNull(),
+  state: text().notNull(),
+  zipCode: text().notNull(),
+  country: text().notNull(),
+  dateOfBirth: date({ mode: "date" }),
+});
---
+++ orders/src/http/server.ts
@@ -0,0 +1,61 @@
+import { fastify } from "fastify";
+import { randomUUID } from "node:crypto";
+import { fastifyCors } from "@fastify/cors";
+import { z } from "zod";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { channels } from "../broker/channels/index.ts";
+import { db } from "../db/client.ts";
+import { schema } from "../db/schema/index.ts";
+import { dispatchOrderCreated } from "../broker/messages/order-created.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+app.register(fastifyCors, { origin: "*" });
+
+app.get("/health", () => {
+  return "OK";
+});
+
+app.post(
+  "/orders",
+  {
+    schema: {
+      body: z.object({
+        amount: z.coerce.number(),
+      }),
+    },
+  },
+  async (request, response) => {
+    const { amount } = request.body;
+
+    console.log("Creating an order with amount", amount);
+
+    const orderId = randomUUID();
+
+    dispatchOrderCreated({
+      orderId,
+      amount,
+      customer: {
+        id: "f07d3453-71c8-4834-84b5-b7a3733740e0",
+      },
+    });
+
+    await db.insert(schema.orders).values({
+      amount,
+      id: orderId,
+      customerId: "f07d3453-71c8-4834-84b5-b7a3733740e0",
+    });
+
+    return response.status(201).send();
+  }
+);
+
+app.listen({ host: "0.0.0.0", port: 3333 }).then(() => {
+  console.log("[Orders] HTTP Server running!");
+});
