22b7fdf0804f50ed0fddf384c4aeb256c81dc21e
Bruno Monteiro <brunomonteiro3@outlook.com.br>
2025-06-07 11:35:19 -0300

    feat: add tacer

--- orders/src/http/server.ts
+++ orders/src/http/server.ts
@@ -1,6 +1,9 @@
+import "@opentelemetry/auto-instrumentations-node/register";
 import { fastify } from "fastify";
 import { randomUUID } from "node:crypto";
 import { fastifyCors } from "@fastify/cors";
+import { trace } from "@opentelemetry/api";
+import { setTimeout } from "node:timers/promises";
 import { z } from "zod";
 import {
   serializerCompiler,
@@ -11,6 +14,7 @@ import { channels } from "../broker/channels/index.ts";
 import { db } from "../db/client.ts";
 import { schema } from "../db/schema/index.ts";
 import { dispatchOrderCreated } from "../broker/messages/order-created.ts";
+import { tracer } from "../tracer/tracer.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -38,6 +42,20 @@ app.post(
 
     const orderId = randomUUID();
 
+    await db.insert(schema.orders).values({
+      amount,
+      id: orderId,
+      customerId: "f07d3453-71c8-4834-84b5-b7a3733740e0",
+    });
+
+    const span = tracer.startSpan("Acho que ta dando ruim");
+
+    await setTimeout(2000);
+
+    span.end();
+
+    trace.getActiveSpan()?.setAttribute("order_id", orderId);
+
     dispatchOrderCreated({
       orderId,
       amount,
@@ -46,16 +64,10 @@ app.post(
       },
     });
 
-    await db.insert(schema.orders).values({
-      amount,
-      id: orderId,
-      customerId: "f07d3453-71c8-4834-84b5-b7a3733740e0",
-    });
-
     return response.status(201).send();
   }
 );
 
-app.listen({ host: "0.0.0.0", port: 3333 }).then(() => {
+app.listen({ host: "0.0.0.0", port: 3334 }).then(() => {
   console.log("[Orders] HTTP Server running!");
 });
